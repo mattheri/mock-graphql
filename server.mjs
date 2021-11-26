0fbc6ea14c7ef19a1dc0bfda18f5c88c942b68f0
Mathieu Thériault <mathieu.theriault89@gmail.com>
2021-11-26 10:35:37 -0500

    Added CORS

--- server.mjs
+++ server.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import jsonGraphqlExpress from "json-graphql-server";
 import fs from "fs";
 import wait from "./wait.mjs";
+import cors from "cors";
 
 const PORT = 3001;
 const ENDPOINT = "/graphql";
@@ -11,6 +12,7 @@ const ENCODING = "utf8";
 const app = express();
 const data = fs.readFileSync(DB_LOCATION, ENCODING);
 
+app.use(cors());
 app.use(wait(2500));
 app.use(ENDPOINT, jsonGraphqlExpress.default(JSON.parse(data)));
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
